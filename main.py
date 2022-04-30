d974871aeac671f23e8f18a9e2787dbf79cbfd8a
rickapps <rick@rickapps.com>
2022-04-29 20:23:50 -0600

    Use jinja to create gameboard

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html', size=3)
+    size = 3
+    boxes = []
+    for i in range(size * size):
+        boxType = 'Normal';
+        if (i+1)%3 == 0:
+            boxType = 'Right'
+        if i >= size * (size-1):
+            if boxType == 'Normal':
+                boxType = 'Bottom'
+            else:
+                boxType = 'Both'
+        boxes.append(boxType)
+
+    return render_template('index.html', size=size, boxes = boxes)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.context_processor
+def utility_processor():
+    def box_type():
+        return 'cat'
+    return dict(box_type=box_type)
+
