ade357c70e5ec19da56c8600e6c739ed8aee3c14
rickapps <rick@rickapps.com>
2022-05-02 15:43:06 -0600

    Init drop down with game size

--- static/js/divboard.js
+++ static/js/divboard.js
@@ -3,3 +3,8 @@ var columns = document.getElementById("gameSize").value;
 var bodyStyles = document.body.style;
 // Set variables in our style sheet.
 bodyStyles.setProperty('--gridSize', columns);
+
+// Initialize the game size drop down on the panel
+var sizeDropDown = document.getElementById("glevel");
+sizeDropDown.value = columns;
+
