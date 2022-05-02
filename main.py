5615f197a9c3850eb26ba04d3d4147b30f9df395
rickapps <rick@rickapps.com>
2022-05-01 20:36:30 -0600

    Post request to change game level

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    size = 3
+def initGame(size):
     boxes = []
+    
     for i in range(size * size):
-        boxType = 'Normal';
-        if (i+1)%3 == 0:
+        boxType = 'Normal'
+        if (i+1)%size == 0:
             boxType = 'Right'
         if i >= size * (size-1):
             if boxType == 'Normal':
@@ -16,7 +15,23 @@ def home():
                 boxType = 'Both'
         boxes.append(boxType)
 
+    return boxes
+
+@app.route("/")
+def home():
+    size = 3
+    boxes = initGame(size)
     return render_template('index.html', size=size, boxes = boxes)
+
+@app.route("/new/", methods = ['POST', 'GET'])
+def newgame():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        size = request.form['glevel']
+        boxes = initGame(int(size))
+        return render_template('index.html',size=size, boxes = boxes)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
