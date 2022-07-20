e3f200f94b5531ef48389dde48d03b543a42893a
rickapps <rick@rickapps.com>
2022-07-19 20:41:05 -0600

    adding namespaces

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,7 @@
 // Respond to events mostly triggered by dotgame.js
 // Functions included in this file are specific to our UI. If we change
 // the UI, we will probably need to modify several of these functions.
-
+var pydots = pydots || {};
 //////////////////////////////////
 // INITIALIZATION 
 //////////////////////////////////
@@ -12,12 +12,12 @@ bodyStyles.setProperty('--gridSize', GAME_SIZE);
 
 // Initialize the game size drop down on the panel
 let sizeDropDown = document.getElementById("glevel");
-sizeDropDown.value = GAME_SIZE;
+if (sizeDropDown) sizeDropDown.value = GAME_SIZE;
 
 // Define a function to change our css theme
 const changeTheme = theme => document.documentElement.className = theme;
 // Set the theme to whatever it was before. We keep that value in localStorage
-let theme = getTheme();
+let theme = pydots.dotgame.getTheme();
 if (theme === null) theme = INIT_THEME;
 changeTheme(theme);
 
@@ -26,42 +26,46 @@ changeTheme(theme);
 //////////////////////////////////
 
 // Page loaded - including style sheets
-window.addEventListener("load", restoreGame);
-
+if (PAGE_NAME == 'game') {
+    window.addEventListener("load", pydots.restoreGame);
+}
 // Add an event listener to all the lines on our gameboard.
 // We need to take action when a user clicks a line.
 let gameboard = document.getElementById("gameboard");
-//let anchors = gameboard.getElementsByTagName("a");
-//for (let anchor of anchors) {
-//    anchor.addEventListener("click", playerMove, false);
-//}
-// Alternate way to listen - not sure if it is better
-gameboard.addEventListener("click", (event) => {
+if (gameboard) {
+  gameboard.addEventListener("click", (event) => {
     if (event.target.tagName.toLowerCase() === "a") {
-      playerMove(event);
+      pydots.playerMove(event);
     }
 });
+}
 
 //////////////////////////////////
 // CHANGE CSS THEME 
 //////////////////////////////////
 let colorDropDown = document.getElementById("gcolors");
-colorDropDown.value = theme;
-colorDropDown.onchange = function () {
-    changeTheme(this.value);
-    // Save the value of the theme so we can retrieve it after a POST
-    storeTheme(this.value)
+if (colorDropDown)
+{
+    colorDropDown.value = theme;
+    colorDropDown.onchange = function () {
+        changeTheme(this.value);
+        // Save the value of the theme so we can retrieve it after a POST
+        pydots.dotgame.storeTheme(this.value)
+    }
 }
 
 //////////////////////////////////
 // START NEW GAME 
 //////////////////////////////////
 let newGame = document.getElementById("gamevals");
-newGame.onsubmit = function() {
-    // Verify it is what our user wants to do
-    alert("This will end your current game.")
-    // Clear our local storage values
-    clearGameValues();
+if (newGame)
+{
+    newGame.onsubmit = function() {
+        // Verify it is what our user wants to do
+        alert("This will end your current game.")
+        // Clear our local storage values
+        pydots.dotgame.clearGameValues();
+    }
 }
 
 ///////////////////////////////////
@@ -70,10 +74,12 @@ newGame.onsubmit = function() {
 // Add event listener to update the gameboard
 // The arg for event is array of [line,box1,box2]
 // Drawing a single line could complete up to two boxes.
-document.addEventListener("drawMove", drawMove, false);
-
-// Add event listener to switch to the other player
-document.addEventListener("switchPlayer", makeMove, false);
+if (PAGE_NAME == 'game')
+{
+    document.addEventListener("drawMove", pydots.drawMove, false);
+    // Add event listener to switch to the other player
+    document.addEventListener("switchPlayer", pydots.dotgame.makeMove, false);
+}
 
 //////////////////////////////////
 // EVENT HANDLERS 
@@ -84,15 +90,15 @@ document.addEventListener("switchPlayer", makeMove, false);
 // the last game played used our default size. To make
 // this more robust, we could use a cookie to store the
 // size of the previous game.
-function restoreGame() {
-    let lines = getLines();
+pydots.restoreGame = function () {
+    let lines = pydots.dotgame.getLines();
     let len = lines.length;
     // Make sure our lines match with current game size
     if (len != 2 * GAME_SIZE * (GAME_SIZE + 1))
     {
         // Clear our stored values and start from scratch
-        clearGameValues();
-        lines = getLines();
+        pydots.dotgame.clearGameValues();
+        lines = pydots.dotgame.getLines();
     }
     // Draw the lines we obtained from storage
     let element;
@@ -104,7 +110,7 @@ function restoreGame() {
         }
     }
     // Check if any squares should be filled.
-    let claims = getClaims();
+    let claims = pydots.dotgame.getClaims();
     len = claims.length;
     for (let i=0; i<len; i++) {
         if (claims[i] >= 0)
@@ -121,23 +127,23 @@ function restoreGame() {
 // Human player has selected a line. Get the line
 // number and validate it with the server. validateMove
 // will draw the line and claim any approprate boxes.
-function playerMove(evt) {
-    let move = validateMove(parseInt(evt.target.id));
+pydots.playerMove = function (evt) {
+    let move = pydots.dotgame.validateMove(parseInt(evt.target.id));
 } 
 
 // Response to drawMove event.
 // Draw a single move on the gameboard.
-function drawMove(evt) {
+pydots.drawMove = function (evt) {
     let move = evt.detail.move;
     if (move[0] >= 0)
     {
         // Store the move
-        pushMove(move)
+        pydots.dotgame.pushMove(move)
         // draw our line
         let line = document.getElementById(move[0].toString());
         line.classList.add("selected");
         // claim any squares
-        if (!claimSquares(move, 1))
+        if (!pydots.claimSquares(move, 1))
         {
             // toggle turn
         }
@@ -152,24 +158,24 @@ function drawMove(evt) {
 // gameboard with the player's color, update the player's
 // score and return true. Otherwise, do nothing and return false.
 // This function can update two squares - move[1] and move[2]
-function claimSquares(move, player) {
+pydots.claimSquares = function (move, player) {
     let points = 0;
     if (move[1] >= 0)
     {
         points += 1;
-        fillSquare(move[1], player);
+        pydots.fillSquare(move[1], player);
     }
     if (move[2] >= 0)
     {
         points += 1;
-        fillSquare(move[2], player);
+        pydots.fillSquare(move[2], player);
     }
-    updateScore(player, points);
+    pydots.dotgame.updateScore(player, points);
     return points > 0;
 }
 
 // Color the square with the player's color
-function fillSquare(boxNum, player)
+pydots.fillSquare = function (boxNum, player)
 {
     let square = document.getElementById("B-" + boxNum);
     let claim = player == 1 ? "claim1" : "claim2";
